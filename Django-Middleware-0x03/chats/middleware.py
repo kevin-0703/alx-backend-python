19468fd64afdee09184616f7141a0456d9609fc5
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-27 14:33:03 +0200

    restricying chat access by time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,7 @@
 import logging
 from datetime import datetime
+from rest_framework.response import Response
+from rest_framework import status
 
 logger = logging.getLogger(__name__)
 handler = logging.FileHandler('requests.log')
@@ -18,3 +20,17 @@ class RequestLoggingMiddleware:
 
         respone = self.get_response(request)
         return respone
+    
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        if 9<= current_hour < 17:
+            return self.get_response(request)
+        else:
+            
+            return Response (
+                {'error': 'Access is restricted to business hours (9 AM to 5 PM).'}, 
+                status=status.HTTP_403_FORBIDDEN
+            )
