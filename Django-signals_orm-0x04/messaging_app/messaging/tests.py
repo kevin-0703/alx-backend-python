833d0f0c35d81900940a1f9a53cdc78717d3eea9
KEVIN NSHUTI <nkevin881@gmail.com>
2025-10-02 15:34:03 +0200

    project setup, creating models and signals

--- Django-signals_orm-0x04/messaging_app/messaging/tests.py
+++ Django-signals_orm-0x04/messaging_app/messaging/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth.models import User
+from messaging.models import Message, Notification
+
+u1 = User.objects.create_user("alice", password="test123")
+u2 = User.objects.create_user("bob", password="test123")
+
+msg = Message.objects.create(sender=u1, receiver=u2, content="Hello Bob!")
