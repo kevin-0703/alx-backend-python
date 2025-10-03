cbcd1a0ebdcc6511f9da1ba1bd867f09a7482877
KEVIN NSHUTI <nkevin881@gmail.com>
2025-10-03 19:16:44 +0200

    finishing up the project

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -4,11 +4,19 @@ from .models import Message
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 # Create your views here.
+@login_required
+def inbox(request):
+    unread_messages = Message.unread.unread_for_user(request.user)
+    return render(request, "inbox.html", {"messages": unread_messages})
+
 @cache_page(60)  # cache for 60 seconds
 def conversation_view(request, user_id):
-    sender = request.user
-    messages = Message.objects.filter(receiver_id=user_id).select_related("sender")
-    return render(request, "conversation.html", {"messages": messages})
+    thread = (
+       Message.objects.filter(receiver_id=user_id, sender=request.user)
+       .select_related("sender", "receiver")
+       .prefetch_related("replies")
+    )
+    return render(request, "conversation.html", {"messages": thread})
 
 @login_required
 def delete_user(request):
