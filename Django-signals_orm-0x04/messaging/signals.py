2a2ea37a38c62ae03fb845f62f88f1fd197d70ef
KEVIN NSHUTI <nkevin881@gmail.com>
2025-10-03 18:46:00 +0200

    finishing up the project

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,7 +1,26 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Message, Notification
+from django.db.models.signals import pre_save
+from .models import Message, Notification, MessageHistory, User
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  
+        old_message = Message.objects.get(pk=instance.pk)
+        if old_message.content != instance.content:
+            MessageHistory.objects.create(
+                message=old_message,
+                old_content=old_message.content
+            )
+            instance.edited = True
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    Notification.objects.filter(user=instance).delete()
+    MessageHistory.objects.filter(message__sender=instance).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Message
 
 # Create your views here.
+@cache_page(60)  # cache for 60 seconds
+def conversation_view(request, user_id):
+    messages = Message.objects.filter(receiver_id=user_id).select_related("sender")
+    return render(request, "conversation.html", {"messages": messages})
