f529838a2a7ad4a640a61997f0ae5e9b93e40264
KEVIN NSHUTI <nkevin881@gmail.com>
2025-10-03 18:41:57 +0200

    finishing up the project

--- Django-signals_orm-0x04/messaging_app/messaging_app/messaging_app/messaging/models.py
+++ Django-signals_orm-0x04/messaging_app/messaging_app/messaging_app/messaging/models.py
@@ -2,14 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class UnreadMessagesManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(receiver=user, read=False).only("id", "content", "timestamp")
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-
+    edited = models.BooleanField(default=False)
+    parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
+    read = models.BooleanField(default=False)
+    objects = models.Manager()  
+    unread_messages = UnreadMessagesManager()  
     def __str__(self):
-        return f"From {self.sender} to {self.recipient} at {self.timestamp}"
+        return f"From {self.sender} to {self.recipient} at {self.timestamp}: {self.content[:20]}"
     
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
@@ -22,3 +30,12 @@ class Notification(models.Model):
     def mark_as_read(self):
         self.read = True
         self.save()
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    changed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for message {self.message.id} at {self.changed_at}"
+    
